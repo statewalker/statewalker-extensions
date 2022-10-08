760bd67d085f46a16ea56db0f639af0479ef6ecf
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2022-10-08 11:26:22 +0200

    fix: properly remove content when there is no more extensions

--- src/SwExtensionPoint.js
+++ src/SwExtensionPoint.js
@@ -3,26 +3,25 @@ import { newUpdatesTracker } from "@statewalker/utils";
 import { replaceDomContent, toDomNode } from "@statewalker/utils-dom";
 
 export default class SwExtensionPoint extends HTMLElement {
-
   static initialize({
     name = "sw-extension-point",
     bind = ({ serviceName, element, ...params }) => {
-      const trackUpdates = newUpdatesTracker()
-        .enter((service) => {
-          if (typeof service === "function") {
-            service = service({ ...params, serviceName, element });
-          }
-          return service;
-        })
+      const trackUpdates = newUpdatesTracker().enter((service) => {
+        if (typeof service === "function") {
+          service = service({ ...params, serviceName, element });
+        }
+        return service;
+      });
       return (list) => {
         const nodes = trackUpdates(list);
         const content = [];
         for (let node of nodes) {
           if (!node) continue;
-          (Array.isArray(node) ? content.push(...node) : content.push(node))
+          Array.isArray(node) ? content.push(...node) : content.push(node);
         }
-        replaceDomContent(element, ...content.map(toDomNode));
-      }
+        if (!content.length) content.push(null);
+        replaceDomContent(element, ...content.map((v) => toDomNode(v)));
+      };
     }
   } = {}) {
     SwExtensionPoint.bind = bind;
